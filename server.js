2a1b3c2ae2430a3fcebe49068d40441b54de96ca
Ashley Richard <ashleymrichard@gmail.com>
2022-04-22 22:49:25 -0700

    New Route View Works

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 // Dependencies
 const express = require("express")
+const Store = require("./models/store.js")
 const app = express()
 require("dotenv").config()
 const mongoose = require("mongoose")
+
+// Middleware
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }))
 
 // Database Connection
 mongoose.connect(process.env.DATABASE_URL, {
@@ -10,10 +15,25 @@ mongoose.connect(process.env.DATABASE_URL, {
     useUnifiedTopology: true,
 })
 
+// Routes / Controllers
+// New
+app.get("/store/new", (req, res) => {
+    res.render("new.ejs")
+})
+
 // Create
 app.post("/store", (req, res) => {
-    res.send("received")
-  })
+    // if (req.body.completed === "on") {
+    //     //if checked, req.body.completed is set to 'on'
+    //     req.body.completed = true
+    //   } else {
+    //     //if not checked, req.body.completed is undefined
+    //     req.body.completed = false
+    //   }
+    Store.create(req.body, (error, createdStore) => {
+        res.send(createdStore)
+    })
+})
 
 // Database Connection Error/Success
 // Define callback functions for various events
