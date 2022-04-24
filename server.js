14eebbd0a32a25d2611b24911b3059290b4dbd5f
Ashley Richard <ashleymrichard@gmail.com>
2022-04-24 11:26:12 -0700

    Express Router Working

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 // Dependencies
 const express = require("express")
-const methodOverride = require("method-override")
 const mongoose = require("mongoose")
-const Product = require("./models/product")
+const methodOverride = require("method-override")
 const app = express()
 require("dotenv").config()
 
@@ -11,79 +10,9 @@ require("dotenv").config()
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 
-
 // Routes / Controllers
-// Seed
-const productSeed = require("./models/productSeed.js")
- 
-app.get("/products/seed", (req, res) => {
-    Product.deleteMany({}, (error, allBooks) => {
-    })
-
-    Product.create(productSeed, (error, data) => {
-        res.redirect("/products")
-    })
-})
-
-// Index
-app.get("/products", (req, res) => {
-    Product.find({}, (error, allProducts) => {
-        res.render("index.ejs", {
-            products: allProducts,
-        })
-    })
-})
-
-// New
-app.get("/products/new", (req, res) => {
-    res.render("new.ejs")
-})
-
-// Delete
-app.delete("/products/:id", (req, res) => {
-    Product.findByIdAndRemove(req.params.id, (err, data) => {
-      res.redirect("/products")
-    })
-})
-
-// Update
-app.put("/products/:id", (req, res) => {
-    Product.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {
-          new: true,
-        },
-        (error, updatedProduct) => {
-          res.redirect(`/products/${req.params.id}`)
-        }
-    )
-})
-
-// Create
-app.post("/products", (req, res) => {
-    Product.create(req.body, (error, createdProduct) => {
-        res.redirect("/products")
-    })
-})
-
-// Edit
-app.get("/products/:id/edit", (req, res) => {
-    Product.findById(req.params.id, (error, foundProduct) => {
-      res.render("edit.ejs", {
-        product: foundProduct,
-        })
-    })
-})
-
-/// Show
-app.get("/products/:id", (req, res) => {
-    Product.findById(req.params.id, (err, foundProduct) => {
-      res.render("show.ejs", {
-        product: foundProduct,
-      })
-    })
-  })
+const productsController = require("./controllers/products")
+app.use("/products", productsController)
 
 // Database Connection
 mongoose.connect(process.env.DATABASE_URL, {
